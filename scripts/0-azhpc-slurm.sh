84c7259c4f57c19cf71bee5caf2d5a7398b364b7
Cedric Husianycia <cehusian@microsoft.com>
2020-06-17 13:41:17 +0200

    azhpc-init first try

--- scripts/0-azhpc-slurm.sh
+++ scripts/0-azhpc-slurm.sh
@@ -28,21 +28,20 @@ echo Copy slurm scripts directory
 cp -a azurehpc/scripts $workdir
 cp -a azurehpc/examples/slurm_autoscale/scripts $workdir
 
-ls -lart config/
-
 cd $workdir
 cp -f ../${admin_user}_id_rsa* .
 chmod 600 ${admin_user}_id_rsa*
 rm scripts/config.json
 ln -s ../config/config.slurmcluster.json scripts/config.json
-ls -lart scripts/
 
 echo -e "Config azhpc" 
-#azhpc-init -c ./config \
-#          -d $workdir \
-#          -v vnet=$vnet,location=$location,resource_group=$resource_group,admin_user=$admin_user,key_vault=$key_vault,install_from=$install_from
+azhpc-init -c ./config \
+          -d $workdir \
+          -v vnet=$vnet,location=$location,resource_group=$resource_group,admin_user=$admin_user,key_vault=$key_vault,install_from=$install_from
 
-#echo cleaning RG $resource_group
-#az group delete -g $resource_group -y
+
+
+echo cleaning RG $resource_group
+az group delete -g $resource_group -y
 echo -e "\e[1;34m script done, ciao bye\033[0m"
 echo "------"
