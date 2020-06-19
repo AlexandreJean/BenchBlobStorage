735002fc3bbc1f9f839b553688f6daac948b2b4d
Cedric Husianycia <cehusian@microsoft.com>
2020-06-19 15:11:07 +0200

    file size

--- scripts/2-writeslocal_stg.sh
+++ scripts/2-writeslocal_stg.sh
@@ -27,9 +27,8 @@ scp $SSH_ARGS -i ./hpcadmin_id_rsa nodelist.txt $admin_user@$headnode_fqdn:
 echo What is /mnt/resource size ?
 disksz=$(ssh $SSH_ARGS -i ./hpcadmin_id_rsa $admin_user@$headnode_fqdn "pdsh -f $numIONodes -w ^azhpc_install_config.vmsscluster/hostlists/compute /bin/df /mnt/resource" | awk '{if ($0 ~ /resource/) {print $5}}' | sort -n | head -1)
 echo $disksz
-filesz=$(echo $(($disksz*80/100/1024/1024)))
-#temporary :
-filesz=$(echo $(($disksz*20/100/1024/1024)))
+filesz=$(($disksz*80/100/1024/1024))
+echo $filesz
 
 echo Create empty files in /mnt/resource directory
 # [hpcadmin@compute000001 resource]$ iozone -i 0 -i 1 -+n -r 1M -t 1 -s 1g -w | grep "Children see throughput for"
