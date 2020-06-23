28659db0629cc6f70535e659a74408fa2134ed10
Cedric Husianycia <cehusian@microsoft.com>
2020-06-23 18:44:49 +0200

    --no-wait

--- scripts/10-delete-rg.sh
+++ scripts/10-delete-rg.sh
@@ -11,4 +11,4 @@ echo -e "Reading inputs inside ./inputs-variables.json"
 echo cleaning up keyvault flag
 az keyvault secret set --vault-name $key_vault --name lock --value 0
 echo cleaning RG $resource_group
-az group delete -g $resource_group -y
+az group delete -g $resource_group -y --no-wait
